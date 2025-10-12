ec50aa5547ac8d44d5b3002b0f22c029d46a9d10
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-10-12 19:04:44 +0300

    Structure about

--- src/js/about.js
+++ src/js/about.js
@@ -1,5 +1,24 @@
-function createAbout() {
+// Import About banner image
+import aboutImage from "../images/shawarma-cook.jpg";
 
+// Create about function
+function createAbout() {
+  const about = document.createElement('div');
+  about.classList.add('about');
+
+  const heading = document.createElement('h2');
+  heading.textContent = 'About us';
+
+  const img = document.createElement('img');
+  img.src = aboutImage;
+  img.alt = 'A cook cutting vegetables for a crispy Shawarma.';
+
+  const paragraph = document.createElement('p');
+  paragraph.textContent = 'At the heart of our kitchen, passion meets precision. Our chef transforms fresh, vibrant vegetables and carefully selected ingredients into a visually stunning and delicious shawarma. Every cut, every spice, every moment in the kitchen is dedicated to creating a culinary experience that\'s as beautiful as it is flavorful';
+
+  about.append(heading, img, paragraph);
+  return about;
 };
 
+// Export function
 export default createAbout;
