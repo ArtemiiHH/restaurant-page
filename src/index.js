a2dda73f540c3272fb70c23c6936f8dacc43a96b
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-10-12 22:14:27 +0300

    Clear out content on load

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 // This file works as a "barrel", so it acts as a single entry point for modules.
 
 import "./styles.css";
-
 import loadPage from "./js/loadPage";
+
+document.body.innerHTML = '';
+const content = document.createElement('div');
+content.id = 'content';
+document.body.appendChild(content);
 
 loadPage();
