70d1ed9d361652aa0272c51114c71cc78af16353
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-10-12 18:47:43 +0300

    Structure home function + add comments

--- src/js/home.js
+++ src/js/home.js
@@ -1,5 +1,30 @@
-function createHome() {
-  
-}
+// Import Home Banner image
+import homeImage from "../images/shawarma.jpg";
 
+// Create homepage function
+function createHome() {
+  const home = document.createElement('div');
+  home.classList.add('home');
+
+  // Title
+  const heading = document.createElement('h1');
+  heading.classList.add('home-title');
+  heading.textContent = 'Welcome to the best Shawarma in the Mediterranean!';
+
+  // Banner image
+  const img = document.createElement('img');
+  img.classList.add('home-img');
+  img.src = homeImage;
+  img.alt = 'A tasty, spicy shawarma with chicken, tomato and letuce.';
+
+  // Description paragraph
+  const paragraph = document.createElement('p');
+  paragraph.textContent = 'The best shawarma in the Mediterranean is found here - tender meat roasted to perfection, wrapped in warm pita with garlic sauce and pickles, delivering pure, flavorful bliss.';
+
+  // Append items
+  home.append(heading, img, paragraph);
+  return home;
+};
+
+// Export function
 export default createHome;
