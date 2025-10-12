22f84bb1e3f0d80112609b885209e10f3a7368ca
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-10-12 22:19:29 +0300

    Assign content

--- src/js/loadPage.js
+++ src/js/loadPage.js
@@ -3,21 +3,7 @@ import createAbout from "./about";
 import createContact from "./contact";
 
 function loadPage() {
-  // window.addEventListener("load", () => {
-  //   createHome();
-  // });
-
-  document.querySelector('.tab-home').addEventListener('click', () => {
-    createHome();
-  });
-
-  document.querySelector('.tab-about').addEventListener('click', () => {
-    createAbout();
-  });
-
-  document.querySelector('.tab-contact').addEventListener('click', () => {
-    createContact();
-  });
+  const content = document.querySelector('#content');
 };
 
 export default loadPage;
