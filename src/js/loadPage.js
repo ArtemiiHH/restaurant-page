f694e53ce68daa24a367271715dbd4eed5d519dd
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-10-14 22:35:03 +0300

    Add event listeners to buttons

--- src/js/loadPage.js
+++ src/js/loadPage.js
@@ -8,6 +8,18 @@ function loadPage() {
   const btnHome = document.querySelector('.tab-home');
   const btnAbout = document.querySelector('.tab-about');
   const btnContact = document.querySelector('.tab-contact');
+
+  btnHome.addEventListener('click', () => {
+    createHome();
+  });
+
+  btnAbout.addEventListener('click', () => {
+    createAbout();
+  });
+
+  btnContact.addEventListener('click', () => {
+    createContact();
+  });
 };
 
 export default loadPage;
