e0a154cd3c0cdd5ab6b908ba1666491955cebb50
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-10-14 23:28:25 +0300

    Add header + nav to DOM and append them

--- src/js/loadPage.js
+++ src/js/loadPage.js
@@ -5,6 +5,9 @@ import createContact from "./contact";
 function loadPage() {
   const content = document.querySelector('#content');
 
+  const header = document.querySelector('header');
+  const nav = document.querySelector('nav');
+
   const btnHome = document.querySelector('.tab-home');
   const btnAbout = document.querySelector('.tab-about');
   const btnContact = document.querySelector('.tab-contact');
@@ -20,6 +23,10 @@ function loadPage() {
   btnContact.addEventListener('click', () => {
     createContact();
   });
+
+  nav.append(btnHome, btnAbout, btnContact);
+  header.appendChild(nav);
+  content.appendChild(header);
 };
 
 export default loadPage;
