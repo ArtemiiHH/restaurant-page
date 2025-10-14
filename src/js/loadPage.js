4ea2d7ba982ddf935b070dd6568608b24129ebcf
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-10-14 22:31:57 +0300

    Add content + buttons to DOM

--- src/js/loadPage.js
+++ src/js/loadPage.js
@@ -4,6 +4,10 @@ import createContact from "./contact";
 
 function loadPage() {
   const content = document.querySelector('#content');
+
+  const btnHome = document.querySelector('.tab-home');
+  const btnAbout = document.querySelector('.tab-about');
+  const btnContact = document.querySelector('.tab-contact');
 };
 
 export default loadPage;
