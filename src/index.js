f0d9b70b2db3630f5416e1ddaa754734a63e3135
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-10-14 22:51:20 +0300

    Remove more broken lines of code

--- src/index.js
+++ src/index.js
@@ -3,8 +3,4 @@
 import "./styles.css";
 import loadPage from "./js/loadPage";
 
-const content = document.createElement('div');
-content.id = 'content';
-document.body.appendChild(content);
-
 loadPage();
