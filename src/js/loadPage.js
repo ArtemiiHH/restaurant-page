8b81b2ea398e9af6c95245bae3fac3e91a88fed0
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-10-14 23:33:06 +0300

    Clear content when button pressed

--- src/js/loadPage.js
+++ src/js/loadPage.js
@@ -3,30 +3,38 @@ import createAbout from "./about";
 import createContact from "./contact";
 
 function loadPage() {
-  const content = document.querySelector('#content');
+  const content = document.querySelector("#content");
 
-  const header = document.querySelector('header');
-  const nav = document.querySelector('nav');
+  const header = document.querySelector("header");
+  const nav = document.querySelector("nav");
 
-  const btnHome = document.querySelector('.tab-home');
-  const btnAbout = document.querySelector('.tab-about');
-  const btnContact = document.querySelector('.tab-contact');
+  const btnHome = document.querySelector(".tab-home");
+  const btnAbout = document.querySelector(".tab-about");
+  const btnContact = document.querySelector(".tab-contact");
 
-  btnHome.addEventListener('click', () => {
+  createHome();
+
+  btnHome.addEventListener("click", () => {
+    content.innerHTML = "";
+    content.appendChild(header);
     createHome();
   });
 
-  btnAbout.addEventListener('click', () => {
+  btnAbout.addEventListener("click", () => {
+    content.innerHTML = "";
+    content.appendChild(header);
     createAbout();
   });
 
-  btnContact.addEventListener('click', () => {
+  btnContact.addEventListener("click", () => {
+    content.innerHTML = "";
+    content.appendChild(header);
     createContact();
   });
 
   nav.append(btnHome, btnAbout, btnContact);
   header.appendChild(nav);
   content.appendChild(header);
-};
+}
 
 export default loadPage;
