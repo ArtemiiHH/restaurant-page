867c4a1b735f978fdff50745f68d69a032213b35
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-10-16 21:47:50 +0300

    Fix loadPage to work correctly

--- src/js/loadPage.js
+++ src/js/loadPage.js
@@ -5,33 +5,25 @@ import createContact from "./contact";
 function loadPage() {
   const content = document.querySelector("#content");
 
-  const header = document.querySelector("header");
-  const nav = document.querySelector("nav");
-
   const btnHome = document.querySelector(".tab-home");
   const btnAbout = document.querySelector(".tab-about");
   const btnContact = document.querySelector(".tab-contact");
 
-  nav.append(btnHome, btnAbout, btnContact);
-  header.appendChild(nav);
-  content.appendChild(header);
+  content.innerHTML = '';
   content.appendChild(createHome());
 
   btnHome.addEventListener("click", () => {
     content.innerHTML = "";
-    content.appendChild(header);
     createHome();
   });
 
   btnAbout.addEventListener("click", () => {
     content.innerHTML = "";
-    content.appendChild(header);
     createAbout();
   });
 
   btnContact.addEventListener("click", () => {
     content.innerHTML = "";
-    content.appendChild(header);
     createContact();
   });
 }
